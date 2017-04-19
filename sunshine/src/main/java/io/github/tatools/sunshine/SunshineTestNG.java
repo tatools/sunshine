b1c3f6015c92b7b660c6443f824bb4867606b9a7
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2017-04-19 18:33:07 +0300

    Remove TestNGConfiguration interface
    Affects #34

--- sunshine/src/main/java/io/github/tatools/sunshine/SunshineTestNG.java
+++ sunshine/src/main/java/io/github/tatools/sunshine/SunshineTestNG.java
@@ -1,12 +1,15 @@
 package io.github.tatools.sunshine;
 
+import org.testng.reporters.JUnitXMLReporter;
+
 /**
  * The {@link SunshineTestNG} class is a main class to run TestNG tests.
  * <p>If no arguments will be provided, then Sunshine will try to find TestNG tests in the CLASSPATH.</p>
  * <p>If an argument will be provided, then Sunshine will run TestNG with given argument.
  * The argument is a path to TestNG configuration file (XML or YAML).</p>
  * <p>The default configuration of TestNG engine is following:
- * <br>- {@link TestNGProduceJunitXml}
+ * <br>- enabled Junit xml reporting
+ * <br>- customized TestNG reports folder
  * </p>
  *
  * @author Dmytro Serdiuk (dmytro.serdiuk@gmail.com)
@@ -21,13 +24,12 @@ public class SunshineTestNG {
         Directory directory = new SmartDirectory(TESTS_OUTPUTS);
         directory.remove();
         if (args != null && args.length > 0) {
-            new TestNGEngine(directory.path(), new PreparedTestNGSuite(args[0]), new TestNGProduceJunitXml()).run();
+            new TestNGEngine(directory.path(), new PreparedTestNGSuite(args[0])).with(new JUnitXMLReporter()).run();
         } else {
             new TestNGEngine(
                     directory.path(),
-                    new CachedTestNGSuite(new LoadableTestNGSuite(new Classpath(), TESTS_OUTPUTS)),
-                    new TestNGProduceJunitXml()
-            ).run();
+                    new CachedTestNGSuite(new LoadableTestNGSuite(new Classpath(), TESTS_OUTPUTS))
+            ).with(new JUnitXMLReporter()).run();
         }
     }
 }
