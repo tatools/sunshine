9ec717349b6103e4cc74f909b5107c690bb346ff
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2017-04-28 20:20:22 +0300

    Rename SunshineJunit4 to Sunshine
    Affects #61

--- sunshine-junit4/src/main/java/io/github/tatools/sunshine/junit4/Sunshine.java
+++ sunshine-junit4/src/main/java/io/github/tatools/sunshine/junit4/Sunshine.java
@@ -5,13 +5,12 @@ import io.github.tatools.sunshine.core.SunshineConfig;
 import io.github.tatools.sunshine.core.SunshineTestsFilter;
 
 /**
- * The {@link SunshineJunit4} class is a main class to run Junit4 tests.
+ * The {@link Sunshine} class is a main class to run Junit4 tests.
  *
  * @author Dmytro Serdiuk (dmytro.serdiuk@gmail.com)
  * @since 18.03.2017
  */
-public class SunshineJunit4 {
-    // @todo #46:30m Rename SunshineJunit4 to Sunshine.
+public class Sunshine {
 
     public static void main(String[] args) {
          // @todo #23:2h Add ability to generate junit xmls by default. Update javadoc in the class.
