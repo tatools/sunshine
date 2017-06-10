244122230770abefa7348cd9a7ff9531168e538f
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2017-06-10 18:51:49 +0300

    Don't modify default TestNG reports directory
    Affects #70

--- sunshine-testng/src/main/java/io/github/tatools/sunshine/testng/TestNGEngine.java
+++ sunshine-testng/src/main/java/io/github/tatools/sunshine/testng/TestNGEngine.java
@@ -5,7 +5,6 @@ import io.github.tatools.sunshine.core.Location;
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
 
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -18,38 +17,40 @@ import java.util.Collections;
 public final class TestNGEngine implements Engine<ITestNGListener> {
 
     private final TestNG engine;
-    private final Path reportsFolder;
     private final TestNGSuite tests;
 
     /**
      * Initializes a newly created {@link TestNGEngine} object so that it represents
-     * an TestNG runner with will be configured with given configuration.
+     * an TestNG runner.
      *
-     * @param reportsFolder a path where TestNG will store the reports
-     * @param tests         an instance of a {@link TestNGSuite} where need to find tests
+     * @param tests an instance of a {@link TestNGSuite} where need to find tests
      */
-    public TestNGEngine(Path reportsFolder, TestNGSuite tests) {
-        this(new TestNG(false), reportsFolder, tests);
+    public TestNGEngine(TestNGSuite tests) {
+        this(new TestNG(false), tests);
     }
 
-    private TestNGEngine(TestNG engine, Path reportsFolder, TestNGSuite tests) {
+    private TestNGEngine(TestNG engine, TestNGSuite tests) {
         this.engine = engine;
-        this.reportsFolder = reportsFolder;
         this.tests = tests;
     }
 
     @Override
     public void run() {
-        engine.setOutputDirectory(reportsFolder.toString());
         engine.setTestSuites(Collections.singletonList(tests.tests().path().toString()));
         engine.run();
         System.exit(engine.getStatus());
     }
 
+    /**
+     * Constructs new {@link TestNGEngine} object with wanted listeners.
+     *
+     * @param listeners an instance (or instances) of engine's listeners
+     * @return an instance of {@link TestNGEngine}
+     */
     @Override
     public TestNGEngine with(ITestNGListener... listeners) {
         final TestNG testNG = new TestNG(false);
         Arrays.stream(listeners).forEach(testNG::addListener);
-        return new TestNGEngine(testNG, reportsFolder, tests);
+        return new TestNGEngine(testNG, tests);
     }
 }
