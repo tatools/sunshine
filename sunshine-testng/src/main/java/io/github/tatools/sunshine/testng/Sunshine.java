f7294ca1d8215f17717b54bf15b64158c14b37e0
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2017-06-10 17:52:04 +0300

    Remove any pre-configured xUnit listeners
    Affects #70

--- sunshine-testng/src/main/java/io/github/tatools/sunshine/testng/Sunshine.java
+++ sunshine-testng/src/main/java/io/github/tatools/sunshine/testng/Sunshine.java
@@ -1,17 +1,12 @@
 package io.github.tatools.sunshine.testng;
 
 import io.github.tatools.sunshine.core.*;
-import org.testng.reporters.JUnitXMLReporter;
 
 /**
  * The {@link Sunshine} class is a main class to run TestNG tests.
  * <p>If no arguments will be provided, then Sunshine will try to find TestNG tests in the CLASSPATH.</p>
  * <p>If an argument will be provided, then Sunshine will run TestNG with given argument.
  * The argument is a path to TestNG configuration file (XML or YAML).</p>
- * <p>The default configuration of TestNG engine is following:
- * <br>- enabled Junit xml reporting
- * <br>- customized TestNG reports folder
- * </p>
  *
  * @author Dmytro Serdiuk (dmytro.serdiuk@gmail.com)
  * @since 18.03.2017
@@ -25,14 +20,14 @@ public class Sunshine {
         Directory directory = new SmartDirectory(path);
         directory.remove();
         if (args != null && args.length > 0) {
-            new TestNGEngine(directory.path(), new PreparedTestNGSuite(args[0])).with(new JUnitXMLReporter()).run();
+            new TestNGEngine(directory.path(), new PreparedTestNGSuite(args[0])).run();
         } else {
             new TestNGEngine(
                     directory.path(),
                     new CachedTestNGSuite(
                             new LoadableTestNGSuite(new Classpath(), path, new SunshineTestsFilter(config))
                     )
-            ).with(new JUnitXMLReporter()).run();
+            ).run();
         }
     }
 }
