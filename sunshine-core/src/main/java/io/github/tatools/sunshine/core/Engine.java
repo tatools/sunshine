5bf57d2cc1a260be294b9ea5e2f3e8749c14426c
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2017-09-16 18:39:47 +0300

    Deprecate Engine
    #81

--- sunshine-core/src/main/java/io/github/tatools/sunshine/core/Engine.java
+++ sunshine-core/src/main/java/io/github/tatools/sunshine/core/Engine.java
@@ -6,14 +6,20 @@ package io.github.tatools.sunshine.core;
  * @author Dmytro Serdiuk (dmytro.serdiuk@gmail.com)
  * @version $Id$
  * @since 0.1
+ * @deprecated since 0.2. Use {@link Kernel} instead.
  */
+@Deprecated
 public interface Engine<Listener> {
 
     /**
      * Runs tests in an engine.
      *
      * @throws EngineException if some error occurs
+     * @deprecated since 0.2. Use {@link Kernel#status()} instead. Please notice that {@link Kernel#status()} doesn't
+     * call {@link System#exit(int)} in the end of tests execution. If you want to do that, please take a look for a
+     * {@link Star}.
      */
+    @Deprecated
     void run() throws EngineException;
 
     /**
@@ -21,6 +27,8 @@ public interface Engine<Listener> {
      *
      * @param listeners an instance (or instances) of engine's listeners
      * @return new instance of an engine
+     * @deprecated since 0.2. Use {@link Kernel#with(Object[])} instead.
      */
+    @Deprecated
     Engine<Listener> with(Listener... listeners);
 }
