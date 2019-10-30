b35e8367e7faf980698bf56af28d260d0a6d70ff
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2019-10-30 21:23:03 +0200

    Add integration tests to test JUnit5 support
    The tests will test the default entry point for JUnit 5 runner.
    
    #160

---
+++ sunshine-junit5-integration-tests/src/main/java/org/tatools/junit5tests/PassedTest.java
@@ -0,0 +1,14 @@
+package org.tatools.junit5tests;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Dmytro Serdiuk
+ * @version $Id$
+ */
+public class PassedTest {
+    @Test
+    public void test() {
+    }
+}
---
+++ sunshine-junit5-integration-tests/src/main/java/org/tatools/junit5tests/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package is used to hold the classes for integration testing of the sunshine-junit5.
+ *
+ * @author Dmytro Serdiuk
+ * @version $Id$
+ */
+package org.tatools.junit4tests;
